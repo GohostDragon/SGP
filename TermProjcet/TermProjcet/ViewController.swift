0745804bfeb91ea8e8db0a53ef19bbdf90b93eec
BullaBulla <EnterYour@Mail.com>
2021-05-05 17:43:22 +0900

    XCODE 프로젝트 생성

---
+++ TermProjcet/TermProjcet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TermProjcet
+//
+//  Created by kpugame on 2021/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
